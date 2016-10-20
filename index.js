170398b3ea49c7e21f39449b198f437a6019536d
Dustin Farris <dustin.farris@gmail.com>
2016-10-20 17:56:52 -0400

    Use injected graphql

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var FileCreator = require('broccoli-file-creator');
 var RSVP = require('rsvp');
 var { transformSchema } = require('cashay');
-var graphql = require('graphql');
 
 module.exports = CashaySchema;
 
@@ -17,18 +16,21 @@ function CashaySchema(graphql, rootSchemaPath, outputFile, _options) {
 
   this.rootSchemaPath = rootSchemaPath;
   this.filename = outputFile;
+  this.graphql = graphql;
 }
 
 CashaySchema.prototype.getContent = function() {
+  // Allow es2015 syntax in our rootSchema file
   require('babel-register')({
     presets: [require.resolve('babel-preset-es2015')]
   });
+  // Load the module and build the rootSchema
+  var graphql = this.graphql;
   var rootSchema = require(this.rootSchemaPath).default(graphql);
-  var _this = this;
   return new RSVP.Promise(function(resolve, reject) {
+    // Transform the schema into a client-safe schema
     transformSchema(rootSchema, graphql.graphql).then(function(schema) {
-      _this.content = "export default " + JSON.stringify(schema);
-      resolve(_this.content);
+      resolve("export default " + JSON.stringify(schema));
     });
   });
 }
